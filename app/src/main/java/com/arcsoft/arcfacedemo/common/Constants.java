026e7a4e37814c34276ebdfc2d9e282b86ad450b
zhouyang <814575934@qq.com>
2019-02-19 16:50:33 +0800

    Initial commit

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "EeTvBNKhCwAbuV88EsBhMHuMuTaTkTFe41TQuDVM9Z97";
+    public static final String SDK_KEY = "55F3FhrLypysxBADsgZCJ52dD32CfeE2RpnePHGzhhrv";
+}
